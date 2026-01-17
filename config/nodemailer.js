2f2dc5ced6c1366ec4f8666a39da54535615096d
Ubuntu <ubuntu@ip-172-31-16-127.eu-north-1.compute.internal>
2026-01-17 16:19:22 +0000

    Tested on EC2: updated on ec2 and tested successfully.

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "smtp.gmail.com",
+  port: 587,
   auth: {
     user: process.env.EMAIL_NODEMAILER,
     pass: process.env.PASS_NODEMAILER
