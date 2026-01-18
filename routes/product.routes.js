ab089183a74ea53d3d445e68351c1aa94ffb7d69
amanyadavv605 <amanmanohar011@gmail.com>
2026-01-18 22:12:49 +0530

    Added product routes

---
+++ routes/product.routes.js
@@ -0,0 +1,22 @@
+import { requireAuth, requireAdmin } from "../middleware/auth.middleware";
+import pool from "../config/db";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/", requireAuth, requireAdmin, async (req, res) => {
+  const { name, description, price, stock, category_id } = req.body;
+
+  if (!name || !price) {
+    return res.status(400).json({ message: "Please fill required fields." });
+  }
+
+  const result = await pool.query(
+    `INSERT INTO products (name, description, price, stock, category_id) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    [name, description, price, stock, category_id],
+  );
+
+  return res.status(201).json({ message: result.rows[0] })
+});
+
+export default router;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import redisClient from "./config/redis.js";
 import pool from "./config/db.js";
 
+import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 
@@ -18,6 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use("/api/products", productRoutes);
 
 app.get("/health/db", async (req, res) => {
   try {
